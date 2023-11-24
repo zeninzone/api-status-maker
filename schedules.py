3b4620a6ee5815f89719bc21cce386e3cc367957
alsaheem <adebisiayomide07@gmail.com>
2023-11-24 02:17:44 +0000

    added v1 features

--- schedules.py
+++
@@ -1,7 +0,0 @@
-import logging
-from utils import check_api_status
-
-def schedulePingApiEndpoint():
-    print("This test runs every 3 seconds")
-    check_api_status("https://youtube.com/health")
-    logging.info('This is an info message')
